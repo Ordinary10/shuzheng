01e5fba31be026b04eadf2200f6cafab9647fa7a
luozhiqiang <luozhiqiang@f08d6516-a156-4429-9f59-a83795bda16a>
2019-09-05 10:27:20 +0000

    采购页面功能完成

--- mini/src/pages/procurement/order-apply/index.vue
+++ mini/src/pages/procurement/order-apply/index.vue
@@ -1,34 +1,74 @@
 <template>
   <div class="pages orderPages">
     <i-toast id="toast" />
-    <div class="no-goods" v-show="orderList.length===0">
-      <image class="img" src="http://test.c.zdxrchina.com/images/wechat/search_bgc.png"></image>
-      <div class="no-goods-text">
-        <span>还没有商品</span><br>
-        <span>请点击新增商品添加</span>
+    <scroll-view scroll-y class="category-list" v-if="categoryList">
+      <div class="category-list-item" data-index="-1" @click="categoryClick" :class="{'category-active':categoryActive == 'all'}">
+        <span style="padding-left: 8px">全部</span>
       </div>
-    </div>
-    <div class="mini-list" v-show="orderList.length>0">
-      <div class="mini-list-item">
-
-      </div>
-      <div class="mini-list-item">
-
-      </div>
-      <div class="mini-list-item">
-
-      </div>
-    </div>
-    <div class="footer">
-      <div class="footer-btn">
-        <picker @change="bindPickerChange" @columnchange="bindColumnChange" :value="goodsIndex" mode="multiSelector" :range="multiArray" range-key="type_name">
-          <div class="picker">
-            <span>新增商品</span>
+      <div
+        class="category-list-item"
+        @click="categoryClick"
+        :data-index="index1"
+        v-for="(item1,index1) in categoryList"
+        :key="item1.id"
+      >
+        <div class="category-1" :class="{'category-active':categoryActive == item1.id}">
+          <span class="flex_1">{{item1.type_name}}</span>
+          <span v-if="item1.children.length>0" data-icon="1"><i-icon data-icon="1" type="enter" size="20"/></span>
+        </div>
+        <div class="category-1-list" @click.stop="" v-if="item1.children.length>0&&category1Show==item1.id">
+          <div
+            class="category-1-list-item"
+            v-for="(item2,index2) in item1.children"
+            :key="item2.type_name"
+            @click="category1Click(index1,index2)"
+            :class="{'category1-active':category1Active == index2}"
+          >
+            <div style="text-align: center">
+              {{item2.type_name}}
+            </div>
           </div>
-        </picker>
+        </div>
       </div>
-      <div class="footer-btn" @click="submitOrder">
-        提交订单
+    </scroll-view>
+    <scroll-view scroll-y class="goods-list" v-if="goodsList.length>0">
+      <div class="goods-item" v-for="(item,index) in goodsList" :key="item.id">
+        <i-card :title="item.name" extra=" " thumb=" ">
+          <div slot="content" class="content">
+            <div>单价：<input type="digit" v-model="item.unit_price">元/{{item.unit}}</div>
+            <div>数量：<input type="number" v-model="item.amount">{{item.unit}}</div>
+          </div>
+          <div slot="footer" class="card-footer">
+            <span></span>
+            <span @click="addOrder(item)" class="add-order-btn">加入订单</span>
+          </div>
+        </i-card>
+      </div>
+    </scroll-view>
+    <div class="goods-list" v-if="goodsList.length==0">
+      <div class="no-goods">该类目暂无商品</div>
+    </div>
+    <div class="order-footer">
+      <div class="order-list" v-if="orderList.length>0&&isSeeOrderList">
+        <div class="order-list-item" v-if="item.isShow" v-for="(item,index) in orderList" :key="item.goods_id">
+          <div>{{item.name}}</div>
+          <div>￥{{item.unit_price}}</div>
+          <div class="amount-change">
+            <span @click="amountSub(item)">-</span>
+            <span>{{item.amount}}</span>
+            <span @click="amountAdd(item)">+</span>
+          </div>
+        </div>
+      </div>
+      <div class="footer-btns">
+        <div class="see-order-list" @click="seeOrderList">
+          <span class="order-count" v-if="orderCount>0">{{orderCount}}</span>
+          <icon class="iconfont iconcaigoudan caigoudan"></icon>
+          <span class="total_amount"><i-icon style="margin-left: 12px;" type="financial_fill" size="24" color="red"/>    {{total_amount}}</span>
+        </div>
+        <div class="submit" @click="submitOrder">
+          提交
+        </div>
       </div>
     </div>
   </div>
@@ -39,65 +79,157 @@
 
     data() {
       return {
-        goodsList: [],
+        last_type_id: null,
+        type_id: '',
+        category1Show: -1,
+        categoryActive: 'all',
+        category1Active:0,
         orderList: [],
-        multiArray: [],
-        goodsIndex: [0,0,0],
+        goodsList: [],
+        categoryList: null,
+        orderCount: 0,
+        total_amount: 0,
+        isSeeOrderList: false
       }
     },
 
     created() {
     },
     onShow() {
+      // this.init()
+      // this.getGoodsList(true)
     },
     mounted(){
+      this.type_id = ''
+      this.last_type_id = null
+      this.category1Show = -1
+      this.categoryActive = 'all'
+      this.category1Active = 0
+      this.orderList = []
+      this.orderCount = 0
+      this.total_amount = 0
+      this.isSeeOrderList = false
       this.init()
+      this.getGoodsList(true)
     },
     methods:{
       init(){
+        this.categoryList = []
         const  _this = this
         _this.$ajax('goods/getGoodsType','',function (res) {
-          _this.goodsList = res.data
-          /**** 利用JSON方法达到深拷贝数组的目的，让vue数据监视能正确识别数组的变化 *******/
-          let _copy_data = JSON.parse(JSON.stringify(res.data))
-          _this.multiArray[0] = _copy_data
-          _this.multiArray[1] = _copy_data[0].children
-          _this.multiArray[2] = _copy_data[0].children[0].children
+          _this.categoryList = res.data
         })
       },
-      bindPickerChange (e) {
-        const _this = this,
-              val = e.mp.detail.value
-        for(let i in _this.goodsIndex) {
-          if(val[i] !== _this.goodsIndex[i]){
+      getGoodsList(isFirst) {
+        const _this =this
+        if(isFirst || (_this.last_type_id!=_this.type_id)) {
+          _this.last_type_id = _this.type_id
+          _this.$ajax('goods/getAllGoodsByType',{type_id:_this.type_id},function (res) {
+            _this.goodsList = res.data
+            // console.log(_this.goodsList)
+          })
+        }
+      },
+      categoryClick(e) {
+        let index = e.mp.currentTarget.dataset.index,
+            icon = e.mp.target.dataset.icon
+        if(index==-1) {
+          this.category1Show = -1
+          this.categoryActive = 'all'
+          this.type_id = ''
+        } else if(icon==1) {
+          this.categoryActive = this.categoryList[index].id
+          this.category1Show = this.categoryList[index].id
+          this.type_id = this.categoryList[index].children[0].id
+        } else if(icon!=1) {
+          this.categoryActive = this.categoryList[index].id
+          this.category1Show = -1
+          this.type_id = this.categoryList[index].id
+        }
+        this.category1Active = 0
+        this.getGoodsList()
+      },
+      category1Click(index1,index2) {
+        this.category1Active = index2
+        this.type_id = this.categoryList[index1].children[index2].id
+        this.getGoodsList()
+      },
+      addOrder(item) {
+        if(item.amount && item.unit_price){
+          this.orderList.push({
+            name: item.name,
+            goods_id:item.id,
+            amount:item.amount,
+            unit_price: item.unit_price,
+            isShow: true
+          })
+          this.total_amount += Number(item.amount) * Number(item.unit_price)
+          this.orderCount ++
+        } else {
+          this.$common.error_tip('请输入商品单价和数量')
+        }
+      },
+      amountSub(item) {
+        let total_amount = 0
+        item.amount--
+        if(item.amount==0){
+          item.isShow = false
+          this.orderCount--
+          this.orderList = JSON.parse(JSON.stringify(this.orderList))
+        }
+        for(let v of this.orderList){
+          if(v.isShow){
+            total_amount += Number(v.amount)*Number(v.unit_price)
+          }
+        }
+        this.total_amount = total_amount
+      },
+      amountAdd(item) {
+        let total_amount = 0
+        item.amount++
+        this.total_amount = 0
+        for(let v of this.orderList){
+          if(v.isShow){
+            total_amount += Number(v.amount)*Number(v.unit_price)
+          }
+        }
+        this.total_amount = total_amount
+      },
+      seeOrderList() {
+        if(this.orderList.length===0 || this.isSeeOrderList == true){
+          this.isSeeOrderList = false
+          return
+        }
+        let _copy_orderList = JSON.parse(JSON.stringify(this.orderList))
+        for(let v of _copy_orderList){
+          if(v.isShow) {
+            this.isSeeOrderList = true
             return
           }
         }
-        let _copy_data = JSON.parse(JSON.stringify(_this.goodsList))
-        console.log(_copy_data)
-        _this.orderList.push(_copy_data[_this.goodsIndex[0]][_this.goodsIndex[1]][_this.goodsIndex[2]])
-        console.log(_this.orderList)
-      },
-      bindColumnChange (e) {
-        const _this = this
-        /**** 利用JSON方法达到深拷贝数组的目的，让vue数据监视能正确识别数组的变化 *******/
-        let _copy_data = JSON.parse(JSON.stringify(_this.goodsList)),
-            _copy_multiArray = JSON.parse(JSON.stringify(_this.multiArray))
-        _this.goodsIndex[e.mp.detail.column] = e.mp.detail.value
-        if(e.mp.detail.column === 0){
-          _this.goodsIndex[1]=0
-          _this.goodsIndex[2]=0
-          _copy_multiArray[1]=_copy_data[_this.goodsIndex[0]].children
-          _copy_multiArray[2]=_copy_data[_this.goodsIndex[0]].children[_this.goodsIndex[1]].children
-
-        } else if(e.mp.detail.column === 1) {
-          _this.goodsIndex[2]=0
-          _copy_multiArray[2]=_copy_data[_this.goodsIndex[0]].children[_this.goodsIndex[1]].children
-        }
-        _this.multiArray = _copy_multiArray
       },
       submitOrder(){
-        console.log(this.orderList)
+        const _this = this
+        let submitOrder = {
+          order_id: 0,
+          data: []
+        }
+        for(let v of this.orderList){
+          if(v.isShow){
+            submitOrder.data.push(v)
+          }
+        }
+        if(submitOrder.data.length === 0){
+          _this.$common.error_tip('未添加任何商品')
+        } else {
+          _this.$ajax('purchaseOrder/apply',submitOrder,function (res) {
+            if(res.code===1){
+              _this.$common.success_tip('订单提交成功',function () {
+                wx.reLaunch({url:'/pages/procurement/main'})
+              })
+            }
+          })
+        }
       }
     }
   }
@@ -105,23 +237,182 @@
 
 <style lang="wxss">
   page{
-    background-color: #F3F3F3;
+    background-color: white;
+    height: 100%;
   }
 </style>
 <style scoped lang="scss">
   .pages{
-    display: flex;
-    .no-goods{
-      margin: auto;
-      .img{
-        width: 198px;
-        height: 100px;
-        margin-bottom: 10px;
-      }
-      .no-goods-text{
-        font-size: 14px;
-        text-align: center;
+    height: 100%;
+    width: 100%;
+    position: relative;
+    .category-list{
+      border-top: 1px solid #eee;
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 80px;
+      box-sizing: border-box;
+      padding-bottom: 60px;
+      background-color: #F3F3F3;
+      height: 100%;
+      .category-list-item{
+        min-height: 36px;
+        line-height: 36px;
+        display: flex;
+        flex-direction: column;
+        font-size: 12px;
         color: #999;
+        .category-1{
+          display: flex;
+          .flex_1{
+            flex: 1;
+            padding-left: 8px;
+          }
+        }
+        .category-1-list{
+          display: flex;
+          flex-direction: column;
+          width: 100%;
+          background-color: #ddd;
+        }
+      }
+      .category-active{
+        background-color: #ddd !important;
+      }
+      .category1-active{
+        color: #3a8ff5 !important;
+      }
+    }
+    .goods-list{
+      border-top: 1px solid #eee;
+      margin-left: 80px;
+      box-sizing: border-box;
+      background-color: white;
+      padding: 0 0 60px 10px;
+      min-height: 100%;
+      .no-goods{
+        margin-top: 20px;
+        text-align: center;
+        font-size: 14px;
+        color: #999;
+      }
+      .goods-item{
+        padding-right: 10px;
+        box-sizing: border-box;
+        margin: 8px 0;
+        width: 283px;
+      }
+      .content{
+        margin-top: 12px;
+        div{
+          display: flex;
+          margin-top: 8px;
+          align-items: center;
+        }
+        input{
+          width: 120px;
+          text-align: center;
+          margin: 0 2px;
+          border-bottom: 1px solid #999;
+        }
+      }
+      .card-footer{
+        display: flex;
+        padding-bottom: 2px;
+        justify-content: space-between;
+        .add-order-btn{
+          padding: 2px 8px;
+          border-radius: 14px;
+          color: white;
+          background-color: #f1338e;
+          &:hover{
+            background-color: #f12126;
+          }
+        }
+      }
+    }
+    .order-footer{
+      position: fixed;
+      bottom: 0;
+      background-color: #595959;
+      height: 42px;
+      width: 100%;
+      z-index: 1000;
+      .order-list{
+        width: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        transform: translateY(-100%);
+        display: flex;
+        flex-direction: column;
+        color: #999;
+        font-size: 14px;
+        background-color: white;
+        .order-list-item{
+          width: 100%;
+          border-top: 1px solid #999;
+          display: flex;
+          padding: 0 24px;
+          box-sizing: border-box;
+          justify-content: space-between;
+          height: 32px;
+          div{
+            min-width: 80px;
+            text-align: center;
+            line-height: 32px;
+          }
+          .amount-change{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            span{
+              width: 28px;
+              height: 20px;
+              border: 1px solid #eee;
+              line-height: 20px;
+              text-align: center;
+            }
+          }
+        }
+      }
+      .footer-btns{
+        display: flex;
+        width: 100%;
+        line-height: 42px;
+        color: #eee;
+        .submit{
+          font-size: 16px;
+          padding: 0 16px;
+          background-color: #818181;
+        }
+        .see-order-list{
+          position: relative;
+          .order-count{
+            position: absolute;
+            top: 3px;
+            left: 30px;
+            border-radius: 50%;
+            width: 16px;
+            height: 16px;
+            text-align: center;
+            line-height: 12px;
+            font-size: 12px;
+            background-color: #f51247;
+            color: white;
+          }
+          flex: 1;
+          padding-left: 16px;
+          box-sizing: border-box;
+          .caigoudan{
+            font-size: 24px;
+            padding-right: 40px;
+          }
+          .total_amount{
+            line-height: 24px;
+          }
+        }
       }
     }
   }
--- mini/src/pages/procurement/order-details/index.vue
+++ mini/src/pages/procurement/order-details/index.vue
@@ -2,9 +2,6 @@
   <div class="pages">
     <i-toast id="toast" />
     <div class="detail-box" v-if="orderDetail">
-      <div class="divider-title">
-        <divider content="基本信息" :css="{'font-size': '17px'}"></divider>
-      </div>
       <div class="dataList">
         <div class="dataItem">
           <span class="dataItemLeft">订单编号：</span>
@@ -12,27 +9,52 @@
         </div>
         <div class="dataItem">
           <span class="dataItemLeft">申请人：</span>
-          <span class="dataItemright">{{orderDetail.uid||'-'}}</span>
+          <span class="dataItemright">{{orderDetail.uname||'-'}}</span>
         </div>
         <div class="dataItem">
-          <span class="dataItemLeft">商品总数：</span>
-          <span class="dataItemright">{{orderDetail.total_count||'0'}}</span>
+          <span class="dataItemLeft">预估金额：</span>
+          <span class="dataItemright">{{orderDetail.estimated_amount||'0'}}元</span>
         </div>
-        <div class="dataItem">
-          <span class="dataItemLeft">商品总价：</span>
-          <span class="dataItemright">{{orderDetail.total_amount||'0'}}</span>
+        <div class="dataItem" v-if="orderDetail.total_amount">
+          <span class="dataItemLeft">实购金额：</span>
+          <span class="dataItemright">{{orderDetail.total_amount}}元</span>
         </div>
         <div class="dataItem">
           <span class="dataItemLeft">申请时间：</span>
           <span class="dataItemright">{{orderDetail.ctime||'-'}}</span>
         </div>
         <div class="dataItem">
+          <span class="dataItemLeft">申请门店：</span>
+          <span class="dataItemright">{{orderDetail.store_name||'-'}}</span>
+        </div>
+        <div class="dataItem">
           <span class="dataItemLeft">状态：</span>
-          <span class="dataItemright">{{orderDetail.status}}</span>
+          <span class="dataItemright">{{orderDetail.status_name}}</span>
         </div>
       </div>
-      <div class="divider-title">
-        <divider content="商品详情" :css="{'font-size': '17px'}"></divider>
+      <div class="goods-list" v-if="orderDetail.detail.length>0">
+        <div class="divider-title">
+          <divider content="商品详情" :css="{'font-size': '17px'}"></divider>
+        </div>
+        <div class="goods-item" v-for="(item,index) in orderDetail.detail" :key="item.id">
+          <i-card :title="item.name" extra=" " thumb=" ">
+            <div slot="content" class="content">
+              <span>预购数量：{{item.apply_amount}}{{item.unit}}</span>
+              <span>预估花费：{{item.estimated_money}}元</span>
+              <span>实购数量：{{item.buy_amount}}{{item.unit}}</span>
+              <span>实购花费：{{item.buy_money}}元</span>
+            </div>
+            <div slot="footer">
+              <div class="status-caozuo">
+                <div v-if="orderDetail.status==='deny'"><span>已拒绝</span></div>
+                <div v-else-if="orderDetail.status==='done'"><span>已入库</span></div>
+                <div v-else-if="orderDetail.status==='buy'"><span class="put-in-storage">入库</span></div>
+                <div v-else-if="orderDetail.status==='pass'"><span class="entry">采购中</span></div>
+                <div v-else-if="orderDetail.status==='apply'"><span>审核中</span></div>
+              </div>
+            </div>
+          </i-card>
+        </div>
       </div>
     </div>
   </div>
@@ -51,6 +73,9 @@
     created() {
     },
     onShow() {
+      this.orderDetail = null
+      this.id = this.$root.$mp.query.id
+      this.getDetail()
     },
     mounted(){
       this.id = this.$root.$mp.query.id
@@ -86,34 +111,60 @@
 </style>
 <style scoped lang="scss">
   .pages{
+    height: 100%;
+    box-sizing: border-box;
     padding-top: 30rpx;
-    .dataList{
-      width: 100%;
-      display: flex;
-      flex-wrap: wrap;
-      justify-content: space-between;
-      .dataItem{
-        width: 100%;
-        min-height: 86rpx;
-        box-sizing: border-box;
-        border-bottom: 2rpx solid #D9D9D9;
-        color: #000;
-        font-size: 34rpx;
-        line-height: 80rpx;
-        position: relative;
-        .dataItemLeft{
-          color: #8A98AC;
-          padding-right: 10rpx;
-        }
-      }
-    }
+    display: flex;
+    flex-direction: column;
     .detail-box{
+      flex: 1;
+      width: 100%;
       background-color: white;
+      box-sizing: border-box;
       box-shadow:0 0 38rpx 11rpx rgba(0, 0, 0, 0.11);
       border-radius:40rpx 40rpx 0 0;
       padding: 0 40rpx;
+      overflow: scroll;
       .divider-title{
         padding: 40rpx 0 16rpx 0;
+      }
+      .dataList{
+        width: 100%;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        .dataItem{
+          width: 100%;
+          min-height: 86rpx;
+          box-sizing: border-box;
+          border-bottom: 2rpx solid #D9D9D9;
+          color: #000;
+          font-size: 34rpx;
+          line-height: 80rpx;
+          position: relative;
+          .dataItemLeft{
+            color: #8A98AC;
+            padding-right: 10rpx;
+          }
+        }
+      }
+      .goods-list{
+        padding-bottom: 12px;
+        .goods-item{
+          margin: 12px 0;
+          .content{
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+            span{
+              margin: 8px;
+            }
+          }
+          .status-caozuo{
+            font-size: 16px;
+            padding-bottom: 12px;
+          }
+        }
       }
     }
   }
