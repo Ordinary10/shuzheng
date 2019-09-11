b90305044e1001b8dfb8f2a167f957f4fd25f964
zhanghongwei <zhanghongwei@f08d6516-a156-4429-9f59-a83795bda16a>
2019-09-11 09:14:25 +0000

    修改删除按钮的样式

--- pc/src/pages/purchase-manage/list/add.vue
+++ pc/src/pages/purchase-manage/list/add.vue
@@ -2,28 +2,33 @@
   <Row v-if="goodsLists">
     <Row v-for="(v,i) in fromdata" :key="v.id">
       <Form :label-width="80">
-        <Col span="8">
-          <div class="ma-nomb-spacing">
-            <FormItem label="商品">
-              <Select @on-change="getfromid" v-model="fromdata[i].goods_id" >
-                <Option v-for="(list,index) in goodsLists" :value="list.id" :key="list.id">{{list.name}}</Option>
-              </Select>
-            </FormItem>
-          </div>
+        <Col span="22">
+          <Col span="8">
+            <div class="ma-nomb-spacing">
+              <FormItem label="商品">
+                <Select @on-change="getfromid" v-model="fromdata[i].goods_id" >
+                  <Option v-for="(list,index) in goodsLists" :value="list.id" :key="list.id">{{list.name}}</Option>
+                </Select>
+              </FormItem>
+            </div>
+          </Col>
+          <Col span="8">
+            <div class="ma-nomb-spacing">
+              <FormItem label="采购数量">
+                <Input v-model="fromdata[i].amount"></Input>
+              </FormItem>
+            </div>
+          </Col>
+          <Col span="8">
+            <div class="ma-nomb-spacing">
+              <FormItem label="采购单价">
+                <Input v-model="fromdata[i].unit_price"></Input>
+              </FormItem>
+            </div>
+          </Col>
         </Col>
-        <Col span="8">
-          <div class="ma-nomb-spacing">
-            <FormItem label="采购数量">
-              <Input v-model="fromdata[i].amount"></Input>
-            </FormItem>
-          </div>
-        </Col>
-        <Col span="8">
-          <div class="ma-nomb-spacing">
-            <FormItem label="采购单价">
-              <Input v-model="fromdata[i].unit_price"></Input>
-            </FormItem>
-          </div>
+        <Col span="2">
+          <div class="iconfont icon_delete iconshanchuqq" @click="goodsarray('delete',i)"></div>
         </Col>
       </Form>
     </Row>
@@ -35,8 +40,6 @@
     <Col span="12">
       <div class="ma-spacing">
         <Button type="success" @click="goodsarray('add')">新增</Button>
-        <Button type="success" @click="goodsarray('delete')">减少</Button>
-
       </div>
     </Col>
   </Row>
@@ -70,17 +73,19 @@
           console.log(val)
         },
         //新增商品
-        goodsarray(judge){
-          let list={
+        goodsarray(judge,index){
+          console.log(index)
+
+          if (judge =='add'){
+            let list={
               goods_id:'',  //商品id
               amount:'',  //采购数量
               unit_price:'' //采购单价
             };
-          if (judge =='add'){
             this.fromdata.push(list)
           } else {
             if (this.fromdata.length>1){
-              this.fromdata.pop()
+              this.fromdata.splice(index,1)
             }else {
               console.log('不能再减少了哦亲')
             }
