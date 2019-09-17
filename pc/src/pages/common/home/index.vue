0e17075dc1d9195cb372d59115571aa77b5f2be7
zhanghongwei <zhanghongwei@f08d6516-a156-4429-9f59-a83795bda16a>
2019-09-17 08:15:29 +0000

    增加欢迎界面

--- pc/src/pages/common/home/index.vue
+++ pc/src/pages/common/home/index.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="content-block">
-      系统统计页（首页）
+      <div class="box">
+        <p class="top-text">欢迎使用蜀蒸后台管理系统</p>
+      </div>
     </div>
 </template>
 
@@ -18,3 +20,19 @@ export default {
   }
 }
 </script>
+<style scoped lang="scss">
+  .content-block{
+    background-image: url("http://zucheguanjia.oss-cn-qingdao.aliyuncs.com/archivesData/15687066387891.png");
+    background-repeat: no-repeat;
+    background-position: center;
+    width: 100%;
+    height: 100%;
+    .top-text{
+      padding: 10%;
+      margin: 0 auto;
+      font-size:18px;
+    }
+  }
+
+
+</style>
